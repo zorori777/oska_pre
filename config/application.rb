ad629802ed4d54f44fbcff2ed9ae6f22b9551679
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-03-07 20:42:45 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module OsakanOldLadies
+  class Application < Rails::Application
+    config.load_defaults 5.1
+
+    config.generators do |g|
+      g.test_framework false
+      g.helper false
+      g.assets false
+    end
+  end
+end
