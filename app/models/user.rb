e08b610a867048775986f346137a2d4cfc0afd4d
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-03-09 13:19:52 +0900

    [add] userとpictureの紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   devise :omniauthable
 
   has_many :social_profiles, dependent: :destroy
+  has_many :pictures
+  
   def social_profile(provider)
     social_profiles.select{ |sp| sp.provider == provider.to_s }.first
   end
